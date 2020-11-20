e0fa058f3a367b0f057feb1e47a122838a796401
军哥哥 <1273460656@qq.com>
2020-11-20 11:05:57 +0800

    选择排序

--- leetcode/code/ls-11.js
+++ leetcode/code/ls-11.js
@@ -1,14 +1,29 @@
 
 // 冒泡排序
+// export default arr => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[i]) {
+//         let temp = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = temp
+//       }
+//     }
+//   }
+// }
 
+// 选择排序
 export default arr => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length - 1; j++) {
+  for (let i = 0, len = arr.length; i < len; i++) {
+    let index = i;
+    for (let j = i + 1; j < len; j++) {
       if (arr[j] < arr[i]) {
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp
+        index = j
       }
     }
+    let temp = arr[i]
+    arr[i] = arr[index]
+    arr[index] = temp;
   }
+  return arr;
 }
