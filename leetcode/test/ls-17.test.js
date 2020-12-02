830ebe27c595e952c11d6216de02d65d40a69e8b
军哥哥 <1273460656@qq.com>
2020-12-02 22:43:42 +0800

    棒球比赛

---
+++ leetcode/test/ls-17.test.js
@@ -0,0 +1,7 @@
+import calPoints from '../code/ls-17.js'
+
+test('calPoints', () => {
+  expect(calPoints(["5","2","C","D","+"])).toBe(30);
+  expect(calPoints(["5","-2","4","C","D","9","+","+"])).toBe(27);
+  expect(calPoints(["1"])).toBe(1);
+})
