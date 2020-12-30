0132e15d509a6c844f814dd06711636cf3b79009
军哥哥 <1273460656@qq.com>
2020-12-31 00:17:02 +0800

    栈

--- leetcode/code/ls-22.js
+++ leetcode/code/ls-22.js
@@ -30,3 +30,30 @@ function decodeStr(str) {
 }
 
 // 用栈数据思想解决这个问题
+function decodeStr(str) {
+  const stack = [];
+  let tempStr = '';
+  for(let i = 0, len = str.length; i < len; i++) {
+    if (str[i] !== ']') {
+      stack.push(str[i]);
+    } else {
+      let p = stack.pop();
+      while(!/\d/g.test(p)) {
+        tempStr = p + tempStr;
+        p = stack.pop();
+        if (/\d/g.test(p)) {
+          console.log(typeof p)
+          t = stack.length > 0 && stack.pop();
+          while(t && /\d/g.test(t)) {
+            p = t + p;
+            t = stack.length > 0 && stack.pop();
+          }
+          t && stack.push(t)
+        } 
+      }
+      stack.push(tempStr.slice(1).repeat(p));
+      tempStr = '';
+    }
+  }
+  return stack.join('');
+}
